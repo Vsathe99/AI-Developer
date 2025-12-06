8c271749c87a3f16688aaf3c440e3648ecb13005
vallabh sathe <vallabhsathe9906@gmail.com>
2025-12-06 14:41:19 +0530

    cors

--- backend/app.js
+++ backend/app.js
@@ -19,7 +19,13 @@ app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
-app.use(cors({origin: 'https://ai-developer-1-w4ca.onrender.com', credentials: true}));
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN || 'http://localhost:5173',
+    credentials: true,
+  })
+);
+
 
 app.use('/users', userRoutes);
 app.use('/projects', projectRoutes);
