6dffad828f35befe7c00833c4bfb3a798912819b
vallabh sathe <vallabhsathe9906@gmail.com>
2025-12-06 14:42:59 +0530

    cors3

--- backend/app.js
+++ backend/app.js
@@ -19,12 +19,15 @@ app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
-<<<<<<< HEAD
+
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN || 'http://localhost:5173',
+    credentials: true,
+  })
+);
 
 
-=======
-app.use(cors({origin: '*', credentials: true}));
->>>>>>> 6e36737ff1450c211f882f25691d4420a21e84cc
 
 app.use('/users', userRoutes);
 app.use('/projects', projectRoutes);
